a599225cd75b2db0050ea7a4a79e5ab94a0bee60
bxs9775 <bxs9775@rit.edu>
2019-02-19 10:30:39 -0500

    Init graph (no axes)

--- main.js
+++ main.js
@@ -17,6 +17,9 @@ let goodSleep = 7.5;
 
 let key = (d) => d.date;  // put code here for key function to join data to visual elements 
 
+let minDate = (data) => d3.min(data,(d) => d.date);
+let maxDate = (data) => d3.timeDay.offset(d3.max(data,(d) => d.date),1);
+
 function rowConverter(d) {
   // put code here for row conversion
   return{
@@ -33,7 +36,37 @@ function initGraph() {
     dataset = data;
 
     console.log(dataset);
-
+    
+    //Creating graph
+    //Init svg
+    svg = d3.select("#chart")
+      .attr('width',w)
+      .attr('height',h);
+    
+    //Create scales
+    xScale = d3.scaleTime()
+      .domain([minDate(dataset),maxDate(dataset)])
+      .range([0,w]); //Note: add padding?
+    yScale = d3.scaleLinear()
+      .domain([0,12])
+      .range([0,h]); //Note: add padding?
+    cScale = d3.scaleLinear()
+      .domain([0,12])
+      .range(['red','orange']);
+    
+    var defaultNum = 7;
+    //Create bars
+    svg.selectAll('rect')
+      .data(dataset,key)
+      .enter()
+      .append('rect')
+      .attr('x',(d) => xScale(d.date))
+      .attr('y',(d) => h - yScale(d.hours_of_sleep))
+      .attr('width',w/defaultNum)
+      .attr('height',(d) => yScale(d.hours_of_sleep))
+      .attr('fill',(d) => cScale(d.hours_of_sleep));
+    //Create axes
+    
   })
 }
 
